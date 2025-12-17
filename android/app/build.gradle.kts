338f8257e857e200fbf66ffee812a0ace437ddc3
Aeron Jhed Lachano <lachano2499@gmail.com>
2025-12-17 13:23:53 +0800

    Add App Icon for iOS application

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.rockies_fitness_app"
+    namespace = "com.example.rockies_fitness"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.rockies_fitness_app"
+        applicationId = "com.example.rockies_fitness"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
